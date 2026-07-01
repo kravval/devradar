9527fcafb74f1bdad75ba74b72f7ba3d400dcc8b
Val <knigochei3333@gmail.com>
2026-07-01 15:39:58 +0300

    Add Spring Boot project skeleton generated via start.spring.io

---
+++ src/main/java/com/devradar/DevradarApplication.java
@@ -0,0 +1,13 @@
+package com.devradar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevradarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevradarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devradar/DevradarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devradar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevradarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
